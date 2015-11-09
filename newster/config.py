55cc872c89344b4dd776d66d42fac235bc8747a5
Dmitry Abramov <diabramo@ya.ru>
2015-11-10 01:04:36 +0300

    gitignore changed

---
+++ newster/config.py
@@ -0,0 +1,7 @@
+guardURL = 'http://content.guardianapis.com/search?' # insert your guardian URL
+nytURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?' # insert your guardian api-key
+key_g = '2k3dpcst8wwt3tru3euv3m3m' # #insert your nyt api-key
+key_nyt = '7e459ffdf221aed9a2de637503c84cda:4:73195448' ' ' # #insert your nyt api-key
+
+api_urls = [guardURL, nytURL]
+api_keys = [key_g, key_nyt] 
